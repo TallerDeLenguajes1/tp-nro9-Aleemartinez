f9f1cb87131c5725471345e7565c91fd902a77f1
Alejandro Martinez <alee.27do@gmail.com>
2019-05-31 02:45:34 -0300

    empezanding

---
+++ TP9/Helpers/SoporteParaConfiguracion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Helpers
+{
+    public static class SoporteParaConfiguracion
+    {
+
+
+        public static void CrearArchivoDeConfiguracion(string dire,string nombredearch)
+        {
+          FileStream fs = new FileStream(nombredearch, FileMode.OpenOrCreate,FileAccess.Write);       
+            
+          BinaryWriter bw = new BinaryWriter(fs);
+           
+          bw.Write(dire);
+            bw.Close();
+        }
+        public static string LeerArchivoDeConfiguracion(string nombredearch)
+        {
+            FileStream fs = new FileStream(nombredearch, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            nombredearch = bw.ReadString();
+            bw.Close();
+            return nombredearch;
+
+        }
+
+
+
+
+    }
+}
---
+++ TP9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Helpers;
+
+namespace TP9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string ruta = @"C:\Users\alee2\Google Drive\FACET_\Nivel 2\Taller de Lenguajes 1\tp-nro9-Aleemartinez";
+            string arch = @"config2.dat";
+            SoporteParaConfiguracion.CrearArchivoDeConfiguracion(ruta,arch);
+            Console.WriteLine(SoporteParaConfiguracion.LeerArchivoDeConfiguracion(arch));
+
+        }
+    }
+}
